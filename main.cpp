dd0dc2316971cee197da5a65c5714e0f63ec6caa
Sdm26 <118051850+Sdm26@users.noreply.github.com>
2022-11-28 00:11:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int c = 0, d = 1, s = 0, n;
-    cin >> n;
-    if(n>=2) {
-        s=c+d;
-        cout<<s<<endl;
-        for (int i = 0; i <= (n-2); i++) {
-            cout <<s<< endl;
-            c = d;
-            d = s;
-            s = c + d;
-            if (s>=n){
-                break;
-            }
-        }
 
+int main() {
+    bool t = true;
+    int s= 1, o=1,n=0;
+    cin >> n;
+     cout << o <<endl;
+    while(s <= n){
+       
+            cout << s << endl;
+            int z = s;
+           s += o;
+            o = z;
+        
     }
     return 0;
 }
