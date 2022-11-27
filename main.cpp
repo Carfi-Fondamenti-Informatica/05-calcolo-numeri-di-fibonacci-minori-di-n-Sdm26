7851ed4981ec7c9a61f513a92a59cd3b251546a3
Sdm26 <118051850+Sdm26@users.noreply.github.com>
2022-11-27 22:30:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,22 +5,14 @@ int main() {
     int c = 0, d = 1, s = 0, n;
     cin >> n;
     if(n>=2) {
-        while(n>=2){
-            s=1;
-            c=0;
-            d=1;
-            cout << s << endl;
-        for (int i = 0; i <= (n-2); i++) {
+     s=c+d;
+       cout<<s<<endl;
+        for (int i = 0; i <= (n-3); i++) {
             cout <<s<< endl;
             c = d;
             d = s;
             s = c + d;
-            }
-        n--;
-        }
-
- }else{
-            break;
-        }
+            }     
+ }
     return 0;
 }
