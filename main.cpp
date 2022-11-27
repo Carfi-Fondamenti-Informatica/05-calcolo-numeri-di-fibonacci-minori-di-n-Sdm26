b521bad17987486a9569252523cff24fc6145c59
Sdm26 <118051850+Sdm26@users.noreply.github.com>
2022-11-27 22:20:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int c = 0, d = 1, s = 0, n;
+    cin >> n;
+    if(n>=2) {
+        s = c + d;
+        cout << s << endl;
+        while(n>=2){
+            s=1;
+            c=0;
+            d=1;
+            cout << s << endl;
+        for (int i = 0; i <= (n-2); i++) {
+            cout <<s<< endl;
+            c = d;
+            d = s;
+            s = c + d;
+            }
+        n--;
+        }
+
+    }
+    return 0;
 }
